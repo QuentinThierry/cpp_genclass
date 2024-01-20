4a157db162c964e3cf1e09e9cf985fe305dbd39f
QuentinThierry <qthierry2@gmail.com>
2024-01-20 15:14:45 +0100

    [FIX] now properly goes in repo directory and now tests if shell rc has already the export line before adding it

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd cpp_genclass/
 SHELL_NAME=$(echo "$SHELL" | rev | cut -d'/' -f1 | rev)
 SHELL_RC="$HOME/.$SHELL_NAME"rc
 
@@ -11,10 +12,16 @@ then
 fi
 mv -f -t "$HOME"/.cpp_genclass/ genclass LICENSE class_cpp_template class_hpp_template main_cpp_template README.md
 
-echo -e "\n""export PATH=\$PATH:$HOME/.cpp_genclass" >> "$SHELL_RC"
-SHELL_LINE_ADDED=$(cat "$SHELL_RC" | wc -l)
+cat "$SHELL_RC" | grep "export PATH=\$PATH:$HOME/.cpp_genclass" > /dev/null
+if [ $? != 0 ]
+then
+	echo -e "\n""export PATH=\$PATH:$HOME/.cpp_genclass" >> "$SHELL_RC"
+	SHELL_LINE_ADDED=$(cat "$SHELL_RC" | wc -l)
+	echo "[LOG] - PATH export variable has been added, see $SHELL_RC line $SHELL_LINE_ADDED"
+else
+	echo "[LOG] - PATH export variable has not been added because it already exists in $SHELL_RC"
+fi
 rm -f install.sh
 
 echo "[LOG] - cpp_class generator has been successfully installed"
-echo "[LOG] - PATH env variable has been modified, see $SHELL_RC line $SHELL_LINE_ADDED"
 echo "[LOG] - Try 'genclass --help' to ensure the installation worked (you might need to restart terminal or 'source' your shell)"
